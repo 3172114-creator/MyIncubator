7853bf096508f7a4c717cef7593998956d5b7517
3172114-creator <3172114@gmail.com>
2026-02-02 23:49:14 +0500

    v23

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from kivy.app import App
 from kivy.uix.button import Button
-class Main(App):
- def build(self): return Button(text='РАБОТАЕТ!')
-Main().run()
+class M(App):
+ def build(self): return Button(text='POBEDA!')
+M().run()
