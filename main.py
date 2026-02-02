418eaf4ff0af867f2d23657c546c435bc9ac8b29
3172114-creator <3172114@gmail.com>
2026-02-02 21:48:13 +0500

    build

--- main.py
+++ main.py
@@ -1,64 +1,35 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
 
-class CalculatorApp(App):
+class CalcApp(App):
     def build(self):
-        self.operators = ["/", "*", "+", "-"]
-        self.last_was_operator = None
-        self.last_button = None
+        self.formula = "0"
+        bl = BoxLayout(orientation='vertical', padding=10)
+        self.lbl = Label(text="0", font_size=40, size_hint=(1, .4), halign="right")
+        bl.add_widget(self.lbl)
+
+        gl = GridLayout(cols=4, spacing=3, size_hint=(1, .6))
+        btns = ["7","8","9","/", "4","5","6","*", "1","2","3","-", "C","0","=","+"]
         
-        main_layout = BoxLayout(orientation="vertical", padding=10, spacing=5)
+        for b in btns:
+            gl.add_widget(Button(text=b, on_press=self.res))
         
-        # Экран вывода
-        self.solution = Label(text="0", font_size=50, halign="right", 
-                              valign="middle", size_hint=(1, 0.4))
-        self.solution.bind(size=self.solution.setter('text_size'))
-        main_layout.add_widget(self.solution)
-        
-        # Сетка кнопок
-        buttons = [
-            ["7", "8", "9", "/"],
-            ["4", "5", "6", "*"],
-            ["1", "2", "3", "-"],
-            [".", "0", "C", "+"],
-        ]
-        
-        grid_layout = GridLayout(cols=4, spacing=5)
-        for row in buttons:
-            for label in row:
-                button = Button(text=label, pos_hint={"center_x": 0.5, "center_y": 0.5})
+        bl.add_widget(gl)
+        return bl
+
+    def res(self, instance):
+        v = instance.text
+        if v == "C": self.formula = "0"
+        elif v == "=":
+            try: self.formula = str(eval(self.formula))
+            except: self.formula = "Ошибка"
+        else:
+            if self.formula == "0": self.formula = v
+            else: self.formula += v
+        self.lbl.text = self.formula
+
+if __name__ == "__main__":
+    CalcApp().run()
